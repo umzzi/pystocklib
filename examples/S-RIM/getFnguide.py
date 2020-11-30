df342c20271ba8cb2b8a3834f14b4585d73df51c
hyunhae.jo <hyunhae.jo@navercorp.com>
2020-11-30 23:10:50 +0900

    excel header, link add

--- examples/S-RIM/getFnguide.py
+++ examples/S-RIM/getFnguide.py
@@ -27,7 +27,7 @@ i = 0
 data = []
 dividend = []
 for acode in mdf.index:
-    # if i == 10: break
+    if i == 10: break
     code = acode[0]
     ticker = acode[1]
     i = i + 1
@@ -90,8 +90,6 @@ for acode in mdf.index:
 
     #영업 이익률이 증가하나?
 
-
-
     net_worth = capital[2]
     if net_worth is not None:
         net_worth = net_worth * 100000000
@@ -105,14 +103,15 @@ for acode in mdf.index:
     prices = [others[2], others[3], others[4]]
     price_level = srim_calculator.get_price_level(cur_price, prices)
 
-    link = "https://finance.naver.com/item/coinfo.nhn?code="+code.replace("A","")
-
-
+    naver_url = "https://finance.naver.com/item/coinfo.nhn?code="+code.replace("A","")
+    link = '=HYPERLINK("' + naver_url + '", "'+code+'")'
+    consen_url = "http://comp.fnguide.com/SVO2/ASP/SVD_Consensus.asp?pGB=1&gicode="+code+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=108&stkGb=701"
+    consen_link = '=HYPERLINK("' + consen_url + '", "'+ticker+'")'
     if price_level > 0:
         data.append(
             {
-                'code': code,
-                'name': ticker,
+                'code': link,
+                'name': consen_link,
                 'link': link,
                 'est_level': price_level,
                 'price': cur_price,
@@ -184,9 +183,28 @@ if i > 0:
     today = date.today()
     filename = "./srim_hh_" + today.strftime("%Y%m%d") + ".xlsx"
 
-    with pd.ExcelWriter(filename) as writer:
-        df2.to_excel(writer, sheet_name="rim")
+    with pd.ExcelWriter(filename ,engine="xlsxwriter") as writer:
+        df2.to_excel(writer, sheet_name="rim", startrow=1, header=False)
         dd2.to_excel(writer, sheet_name="dividend")
 
+        workbook  = writer.book
+        worksheet = writer.sheets['rim']
+        worksheet2 = writer.sheets['dividend']
+
+        # Add a header format.
+        header_format = workbook.add_format({
+            'bold': True,
+            'text_wrap': True,
+            'valign': 'top',
+            'fg_color': '#D7E4BC',
+            'border': 1})
+
+        # Write the column headers with the defined format.
+        for col_num, value in enumerate(df2.columns.values):
+            worksheet.write(0, col_num + 1, value, header_format)
+            worksheet2.write(0, col_num + 1, value, header_format)
+
+        # Close the Pandas Excel writer and output the Excel file.
+        # writer.save()
 
 
